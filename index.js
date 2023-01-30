17b5be958ea2b3de49d9ad5b6e5b66b1bcc66974
Cl84Dev <cl84dev@yandex.com>
2023-01-30 11:51:26 -0300

    Add header on CORS

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ app.use(
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "*");
   app.use(cors());
   next();
 });
